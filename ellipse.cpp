04a959dd4135cbe2340ba6b2a61a45502dd07f46
luccart <luc.cart03@gmail.com>
2024-03-08 14:21:26 -0600

    Defined ellipse.h

--- ellipse.cpp
+++ ellipse.cpp
@@ -1,1 +1,5 @@
+//Created by Lucas Carter 3/8/24
 #include "ellipse.h"
+
+Ellipse::Ellipse()
+        :Shape()
--- ellipse.h
+++ ellipse.h
@@ -1,3 +1,10 @@
+//Created by Lucas Carter 3/8/24
 #pragma once
 #include "shape.h"
 
+class Ellipse : public Shape {
+    void draw (Canvas& c) override;
+protected:
+    std::vector<int> pts;
+    int a,b;
+};
