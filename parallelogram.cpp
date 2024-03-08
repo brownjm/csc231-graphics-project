9af4657ec20f924772bf976a56fac59edfec480c
anovak <anovak610@gmail.com>
2024-03-08 14:10:16 -0600

    Added parallelogram.cpp and rectangle.cpp

---
+++ parallelogram.cpp
@@ -0,0 +1,20 @@
+//
+// Created by anova on 3/8/2024.
+//
+
+#include "parallelogram.h"
+
+double Parallelogram::area() const {
+    return base * height;
+}
+
+double Parallelogram::perimeter() const {
+    return 2 * (base + side);
+}
+
+Parallelogram::Parallelogram(const std::string& color, double base, double height, double side)
+        : Shape{"parallelogram", color}, base{base}, height{height}, side{side} {
+    if (base <=0 || height <= 0 || side <= 0) {
+        throw std::runtime_error("The number entered cannot be zero or less than zero");
+    }
+}
