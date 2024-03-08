e64d711ced46b4895c18826fe107c525fcde330c
abelje <161378812+abelje@users.noreply.github.com>
2024-03-08 14:21:34 -0600

    Edits to triangle files

--- triangle.cpp
+++ triangle.cpp
@@ -1,3 +1,4 @@
+// Made by: Jonathan 03/08/24
 #include "triangle.h"
 
 void Triangle::draw(Canvas& c) {
--- triangle.h
+++ triangle.h
@@ -1,8 +1,10 @@
+// Made by: Jonathan 03/08/24
+#pragma once
 #include "shape.h"
 #include "vector"
 
 class Triangle : public Shape {
     void draw (Canvas& c) override;
-protected:
+private:
     std::vector<int> pts;
 };
