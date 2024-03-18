417f5ef59653f516c3e41f99181d5b848559e3d8
Martian1648 <Martian1648@outlook.com>
2024-03-18 14:22:47 -0500

    updated stuff

--- parrot.h
+++ parrot.h
@@ -7,9 +7,9 @@
 //repeats what was said to it
 class Parrot : public Canvas {
 public:
-    void draw_polygon(std::vector<Point> pts, style s) override;
-    void draw_polyline(std::vector<Point> pts, style s) override;
-    void draw_ellipse(Point center, Point radii, style s) override;
+    void draw_polygon(std::vector<Point> pts, Style s) override;
+    void draw_polyline(std::vector<Point> pts, Style s) override;
+    void draw_ellipse(Point center, Point radii, Style s) override;
     void save(std::string filename);
     Parrot(Point size):size{size}{}
 private:
