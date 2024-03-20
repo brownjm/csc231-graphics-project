525770b98471d8f1cef5e64ed49979eecfc9bb36
lordspelde <rwspelde15@gmail.com>
2024-03-20 14:19:31 -0500

    Created svg.h

---
+++ svg.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+
+#include "canvas.h"
+
+class SVG : Canvas {
+public:
+    void draw_polygon(std::vector<Point> pts, Style s) override;
+    void draw_polyline(std::vector<Point> pts, Style s) override;
+    void draw_ellipse(Point center, Point radii, Style s) override;
+
+    void save(std::string& filename);
+
+private:
+
+};
