6aade6be53d50e33eabeed1e57052528e35b9d8d
abelje <161378812+abelje@users.noreply.github.com>
2024-03-20 19:29:26 -0500

    Created point constructor to load into vector.

--- triangle.cpp
+++ triangle.cpp
@@ -4,3 +4,11 @@
 void Triangle::draw(Canvas& c) {
     c.draw_polygon(pts, style);
 }
+
+Point Triangle::p(int x, int y) {
+    return Point(x, y);
+}
+
+void Triangle::pt_add(Point p) {
+    pts.push_back(p);
+}
--- triangle.h
+++ triangle.h
@@ -2,9 +2,12 @@
 #pragma once
 #include "shape.h"
 #include "vector"
+#include "point.h"
 
 class Triangle : public Shape {
     void draw (Canvas& c) override;
 private:
     std::vector<Point> pts;
+    Point p (int x, int y);
+    void pt_add(Point p);
 };
