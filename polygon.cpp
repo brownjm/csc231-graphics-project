86ccab96ba4248d680f5cbf81b058faf3afbeba0
megangrow <megangrow05@gmail.com>
2024-03-22 13:53:23 -0500

    polygon updates

--- polygon.cpp
+++ polygon.cpp
@@ -1,6 +1,9 @@
 #include "polygon.h"
 //Megan 3/8
 
+Polygon::Polygon(vector<Point> pts, Style style)
+    :pts{pts}, style{style} {}
+
 void Polygon::draw(Canvas& c) {
     c.draw_polygon(pts, style);
 }
--- polygon.h
+++ polygon.h
@@ -1,10 +1,11 @@
 #pragma once
 #include "style.h"
+#include "shape.h"
 //Megan 3/8
 
 class Polygon : public Shape {
 public:
-    Polygon (Point start, int sides);
+    Polygon(vector<Point> pts, Style style);
     void draw(Canvas& c) override;
 private:
     std::vector<Point> pts;
