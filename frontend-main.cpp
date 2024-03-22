584b161ebf8204f2b2dcb15e13bdc0cb979b19c3
megangrow <megangrow05@gmail.com>
2024-03-22 14:14:30 -0500

    finish polygon - test constructor in main

--- frontend-main.cpp
+++ frontend-main.cpp
@@ -7,4 +7,9 @@ int main() {
     Triangle t{};
     t.draw(svg);
     svg.save("image.svg");
+
+    Parrot parrot{{200, 100}};
+    Polygon p{{{30, 50}, {40, 60}, {20, 40}, {30, 60}, {70, 50}}, polygon_style};
+    p.draw(parrot);
+
 }
--- polygon.cpp
+++ polygon.cpp
@@ -2,9 +2,7 @@
 //Megan 3/8
 
 Polygon::Polygon(vector<Point> pts, Style style)
-    :pts{pts}, style{style} {
-
-}
+    :pts{pts}, style{style} {}
 
 void Polygon::draw(Canvas& c) {
     c.draw_polygon(pts, style);
