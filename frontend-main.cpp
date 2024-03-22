9d9885b34fd16ec330c5f4de76aa65eac5294530
luccart <luc.cart03@gmail.com>
2024-03-22 14:18:55 -0500

    testing frontend-main.cpp - doesn't work

--- frontend-main.cpp
+++ frontend-main.cpp
@@ -1,10 +1,14 @@
-#include "shape.h"
 #include "parrot.h"
 #include <vector>
+#include <iostream>
+#include "ellipse.h"
 
 int main() {
-    SVG svg{100, 100};
-    Triangle t{};
-    t.draw(svg);
-    svg.save("image.svg");
+
+    Parrot p{{200, 200}};
+
+    Ellipse e{{50, 50},{4,5}};
+    e.style.width = 4;
+    e.draw(p);
+    p.save("image.svg");
 }
