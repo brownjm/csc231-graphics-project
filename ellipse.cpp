4404ace13e680a90abaab80b96087e97a30b0624
luccart <luc.cart03@gmail.com>
2024-03-22 12:52:33 -0500

    updated ellipse.cpp/.h

--- ellipse.cpp
+++ ellipse.cpp
@@ -3,4 +3,7 @@
 
 Ellipse (int x, int y, int rx, int ry) : x(x), y(y), rx(rx), ry(ry) {}
 
-Canvas::draw_ellipse()
+void Ellipse::draw() {
+    Canvas canvas;
+    canvas.draw_ellipse(x, y, rx, ry);
+}
--- ellipse.h
+++ ellipse.h
@@ -5,7 +5,7 @@
 class Ellipse : public Shape {
 public:
 
-    Ellipse (int x, int y, int rx, int ry) : x(x), y(y), rx(rx), ry(ry) {}
+    Ellipse (int x, int y, int rx, int ry);
     //where pts is the midpoint and rx is the horizontal radius and ry is the vertical radius
 
     void draw (Canvas& c) override;
