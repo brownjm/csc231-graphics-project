ea43fd8d80052ad46921adcc0320b48d603e0234
LJ <loonjunior@gmail.com>
2024-03-23 13:39:58 -0500

    Finished draw_polygon, edited SVG save to be a const reference, have a preview of draw_polygon in Backend-main and it builds

--- backend-main.cpp
+++ backend-main.cpp
@@ -1,1 +1,17 @@
-int main() {}
+#include "svg.h"
+#include "style.h"
+int main() {
+    std::vector<Point> pts;
+    Point point{2.0, 4.5};
+    pts.push_back(point);
+    Point point1{3.0, 4.5};
+    pts.push_back(point1);
+    Point point2{3.0, 2.5};
+    pts.push_back(point2);
+    Point point3{2.0, 2.5};
+    pts.push_back(point3);
+    Style c;
+
+    SVG s(400, 600);
+    s.draw_polygon(pts, c);
+}
--- svg.cpp
+++ svg.cpp
@@ -6,8 +6,8 @@
 std::string quote(const std::string& s);
 
 // Pair to svg headers; pair<header, value>
-std::string points_to_string(const std::pair<std::string, std::string>& p); // string + string
-std::string points_to_string(const std::pair<std::string, double>& p); // string + double
+std::string pair_to_string(const std::pair<std::string, std::string>& p); // string + string
+std::string pair_to_string(const std::pair<std::string, double>& p); // string + double
 
 void SVG::draw_ellipse(Point center, Point radii, Style s) {
 
@@ -18,13 +18,15 @@ void SVG::draw_polyline(std::vector<Point> pts, Style s) {
 }
 
 void SVG::draw_polygon(std::vector<Point> pts, Style s) {
-    std::string line;
+    std::string line {"polyline"};
     for (Point point : pts) {
-        line += std::to_string(point.x) + " " + std::to_string(point.y) + " ";
+        line += quote(std::to_string(point.x) + " " + std::to_string(point.y) + " ") + "\n";
+        line += "stroke=" + quote(s.border_color) + "fill=" + quote(s.fill_color) + "stroke-width=" + quote(std::to_string(s.border_thickness));
     }
+    data.push_back(line);
 }
 
-void SVG::save(std::string& filename) {
+void SVG::save(const std::string& filename) {
     std::ofstream output {filename};
     output << "<svg ";
 }
