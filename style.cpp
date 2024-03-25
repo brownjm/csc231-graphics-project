7c48a805caad545ffdf9aa147487f5a35fa7b633
lordspelde <rwspelde15@gmail.com>
2024-03-25 14:01:17 -0500

    Implemented `to_string()`

--- style.cpp
+++ style.cpp
@@ -1,1 +1,11 @@
 #include "style.h"
+
+std::string quote(const std::string& s) {
+    return "\"" + s + "\"";
+}
+
+std::string Style::to_string() const {
+    return " stroke=" + quote(border_color)
+           + " fill=" + quote(fill_color)
+           + " stroke-width=" + quote(std::to_string(border_thickness));
+}
