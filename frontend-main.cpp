923a85c58706c2360a3339f54142a2f59464ddad
megangrow <megangrow05@gmail.com>
2024-03-25 13:57:29 -0500

    add polygon testing to frontend main

--- frontend-main.cpp
+++ frontend-main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <iostream>
 #include "ellipse.h"
+#include "polygon.h"
 
 int main() {
     Parrot p{{200, 200}};
@@ -10,4 +11,7 @@ int main() {
     Ellipse e{{50, 50},{4,5}};
     e.draw(p);
     p.save("image.svg");
+
+    Polygon g{{{10, 30}, {50, 30}, {50, 20}}};
+    g.draw(p);
 }
