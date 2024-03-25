107e32962ebbecc42cf0984e3d2bd2dafb54dadd
silas <silasebeling8@gmail.com>
2024-03-25 13:34:05 -0700

    commit polyline-wrote polyline's functions

--- polyline.cpp
+++ polyline.cpp
@@ -6,3 +6,11 @@ std::vector<int> pts;
 void Polyline::draw(Canvas& c) {
     c.draw_polyline(pts, style);
 };
+
+Point Polyline::p(int x, int y) {
+    return Point (x,y);
+}
+
+void Polyline::pt_add(Point p) {
+    pts.push_back(p);
+}
