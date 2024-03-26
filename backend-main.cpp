5ff06d58e3542a11dd1ed2e99a1fca6b696841a3
derpy <cashawn.denton@mail.wlc.edu>
2024-03-26 15:05:54 -0500

    started writing save

--- backend-main.cpp
+++ backend-main.cpp
@@ -1,1 +1,10 @@
-int main() {}
+#include "svg.h"
+
+int main() {
+    SVG svg{300, 500};
+    svg.draw_ellipse({150,200}, {50,50}, Style{});
+    svg.draw_ellipse({250,300}, {50,50}, Style{});
+
+    svg.save("filedagainname.svg");
+
+}
--- svg.cpp
+++ svg.cpp
@@ -9,20 +9,45 @@ std::string points_to_string(const std::pair<std::string, std::string>& p); // s
 std::string points_to_string(const std::pair<std::string, double>& p); // string + double
 
 void SVG::draw_ellipse(Point center, Point radii, Style s) {
+    std::string entry = "<ellipse rx=" + quote(std::to_string(radii.x))
+                        + " ry=" + quote(std::to_string(radii.y))
+                        + " cx=" + quote(std::to_string(center.x))
+                        + " cy=" + quote(std::to_string(center.y))
+                        + s.to_string()
+                        + " />";
 
+    data.push_back(entry);
 }
 
 void SVG::draw_polyline(std::vector<Point> pts, Style s) {
-
+    std::string line {"polyline points="};
+    std::string points;
+    for(Point pt : pts){
+        points += std::to_string(pt.x) +","+std::to_string(pt.y)+" ";
+    }
+    quote(points);
+    line += points;
+    line += "stroke=" + quote(s.border_color) + "fill=" + quote(s.fill_color) + "stroke-width=" + quote(std::to_string(s.border_thickness));
+    data.push_back(line);
 }
 
 void SVG::draw_polygon(std::vector<Point> pts, Style s) {
+    std::string line {"polygon points= "};
+    for (Point point : pts) {
+        line += (std::to_string(point.x) + " " + std::to_string(point.y) + " ");
+    }
+    line += "\n\tstroke=" + quote(s.border_color) + " fill=" + quote(s.fill_color) + " stroke-width=" + quote(std::to_string(s.border_thickness));
 
+    data.push_back(line);
 }
 
 void SVG::save(const std::string& filename) {
     std::ofstream output {filename};
-    output << "<svg ";
+    output << "<svg width = " << quote(std::to_string(width)) << " height = " << std::to_string(height) ;
+    for(std::string line : data) {
+        output << line << '\n';
+    }
+    output << "</svg>\n";
 }
 
 SVG::SVG(int width, int height): width {width}, height {height} {}
