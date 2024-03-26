f6ad13f80b0dda5589f38a82c6c94ffda771f262
derpy <cashawn.denton@mail.wlc.edu>
2024-03-26 14:46:45 -0500

    changed the data thing to a vector thing

--- svg.cpp
+++ svg.cpp
@@ -1,59 +1,23 @@
 #include <fstream>
-#include <format>
-
 #include "svg.h"
-#include <iostream>
 
 // Misc. Helpers
 std::string quote(const std::string& s);
 
 // Pair to svg headers; pair<header, value>
-std::string pair_to_string(const std::pair<std::string, std::string>& p); // string + string
-std::string pair_to_string(const std::pair<std::string, double>& p); // string + double
+std::string points_to_string(const std::pair<std::string, std::string>& p); // string + string
+std::string points_to_string(const std::pair<std::string, double>& p); // string + double
 
 void SVG::draw_ellipse(Point center, Point radii, Style s) {
-    /* // std::format() is only c++ v. 20 or higher (project runs v. 17)
-    std::string entry = std::format(
-            R"(<ellipse rx="{}" ry="{}" cx="{}" cy="{} />")",
-            std::to_string(radii.x),
-            std::to_string(radii.y),
-            std::to_string(center.x),
-            std::to_string(center.x)
-    );
-    */
 
-    std::string entry = "<ellipse rx=" + quote(std::to_string(radii.x))
-            + " ry=" + quote(std::to_string(radii.y))
-            + " cx=" + quote(std::to_string(center.x))
-            + " cy=" + quote(std::to_string(center.y))
-            + s.to_string()
-            + " />";
-
-    data.push_back(entry);
 }
 
 void SVG::draw_polyline(std::vector<Point> pts, Style s) {
-    std::string line {"polyline points="};
-    std::string points;
-    for(Point pt : pts){
-        points += std::to_string(pt.x) +","+std::to_string(pt.y)+" ";
-    }
-    line += quote(points);
-    line += s.to_string();
-    data.push_back(line);
+
 }
 
 void SVG::draw_polygon(std::vector<Point> pts, Style s) {
-    std::string points;
-    for (Point point : pts) {
-        points += (std::to_string(point.x) + " " + std::to_string(point.y) + " ");
-    }
-    std::string line {"<polygon points="};
-    line += quote(points);
-    line += s.to_string();
-    line += "/>";
 
-    data.push_back(line);
 }
 
 void SVG::save(const std::string& filename) {
@@ -63,20 +27,14 @@ void SVG::save(const std::string& filename) {
 
 SVG::SVG(int width, int height): width {width}, height {height} {}
 
-void SVG::print() {
-    for (const std::string& d :data) {
-        std::cout << d << "\n";
-    }
-}
-
 std::string quote(const std::string& s) {
     return "\"" + s + "\"";
 }
 
-std::string pair_to_string(const std::pair<std::string, std::string>& p) {
+std::string points_to_string(const std::pair<std::string, std::string>& p) {
     return p.first + "=" + quote(p.second);
 }
 
-std::string pair_to_string(const std::pair<std::string, double>& p) {
+std::string points_to_string(const std::pair<std::string, double>& p) {
     return p.first + "=" + std::to_string(p.second);
 }
