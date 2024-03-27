258765ad51f2351805baaecad76b5d855b592b6d
silas <silasebeling8@gmail.com>
2024-03-27 13:03:29 -0700

    commit polyline-wrote polyline's functions and added pentagon shape

--- frontend-main.cpp
+++ frontend-main.cpp
@@ -5,6 +5,8 @@
 #include "ellipse.h"
 #include "triangle.h"
 #include "flower.h"
+#include "polyline.h"
+#include "pentagon.h"
 
 int main() {
     Parrot p{{200, 200}};
@@ -15,6 +17,12 @@ int main() {
 
     Triangle t{{0,0}, {2,2}, {5,0}};
     t.draw(p);
+
+    Polyline pl{{{0,0}, {1,2}}};
+    pl.draw(p);
+
+    Pentagon pg{{3,2}, {5,2}, {6,4}, {4,5}, {2,4}};
+    pg.draw(p);
 
     Flower f{{10, 10}, 10.0};
     f.draw(p);
