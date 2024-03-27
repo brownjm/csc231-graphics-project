4961d0420e8a713ab25b551c6d3950c915f6ba5a
derpy <cashawn.denton@mail.wlc.edu>
2024-03-27 13:35:00 -0500

    save filednamesvg thing added shapes can be drawn now i think

--- backend-main.cpp
+++ backend-main.cpp
@@ -3,8 +3,7 @@
 int main() {
     SVG svg{300, 500};
     svg.draw_ellipse({150,200}, {50,50}, Style{});
-    svg.draw_ellipse({250,300}, {50,50}, Style{});
-
+    svg.draw_polygon();
     svg.save("filedagainname.svg");
 
 }
