e606d6bc887721604561ff83dd19c450f54ee5c6
Martian1648 <Martian1648@outlook.com>
2024-03-27 13:37:31 -0500

    polyline more stuff

--- backend-main.cpp
+++ backend-main.cpp
@@ -3,7 +3,17 @@
 int main() {
     SVG svg{300, 500};
     svg.draw_ellipse({150,200}, {50,50}, Style{});
-    svg.draw_polygon();
+    std::vector<Point> pts;
+    Point point{2.0, 4.5};
+    pts.push_back(point);
+    Point point1{3.0, 4.5};
+    pts.push_back(point1);
+    Point point2{3.0, 2.5};
+    pts.push_back(point2);
+    Point point3{2.0, 2.5};
+    pts.push_back(point3);
+    Style c;
+
     svg.save("filedagainname.svg");
 
 }
